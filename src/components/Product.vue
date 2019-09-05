06f096f6d14406bf3a6ab33a7f2b9f83b3c7abfe
Gokhan Cinar <fegokhan@gmail.com>
2019-09-05 20:20:31 +0300

    first commit, hola

---
+++ src/components/Product.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="col-md-2 card">
+      <slot></slot>
+    </div>
+</template>
+<script>
+   export  default  {
+     data : function () {
+        return {
+
+        }
+     }
+   }
+</script>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,30 @@
+<template>
+   <div>
+     <div class="progress">
+       <div class="progress-bar bg-info" role="progressbar" :style="{ width : progress + '%' }" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+         {{ currentItem }}/10
+       </div>
+     </div>
+   </div>
+</template>
+
+<script>
+
+  import  { eventBus } from "../main";
+
+  export default {
+    data : function () {
+        return {
+            currentItem : 0,
+            progress : 0,
+        }
+    },
+    created() {
+      eventBus.$on('progressBarUpdated', (productListCount) => {
+          this.currentItem = productListCount;
+          this.progress = productListCount * 10;
+      })
+    }
+  }
+
+</script>
